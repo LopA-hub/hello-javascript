72abe3438ca9e81a8ae36e5070840ed25a34a003
LopA-hub <juanfrancini@gmail.com>
2025-05-07 11:07:23 -0300

    Update 03-beginner-exercises.js

--- Basic/03-beginner-exercises.js
+++ Basic/03-beginner-exercises.js
@@ -4,16 +4,58 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=4733
 */
 
 // 1. Escribe un comentario en una línea
-
+// hola Viper //
 // 2. Escribe un comentario en varias líneas
-
+/* Hola Viper
+soy lopa practicanding */
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+let myName = "Juan"
+let myalias = "LopA"
+let myemail = "lopa@tuvieja.com"
+
+let age = 35
+let height = 1.74
+
+let isStudent = true
+let isTeacher = false
+
+let undefinedValue
+
+let nullValue = null
+
+let mysymbol = Symbol("mysymbol")
+
+let myBigInt = BigInt(123456789012345678901234567890)
+let myBigInt2 = 123456789012345678901234567890n
 
 // 4. Imprime por consola el valor de todas las variables
+console.log (typeof myName)
+console.log (typeof myalias)
+console.log(typeof email)
+
+console.log(typeof age)
+console.log(typeof height)
+
+console.log(typeof isTeacher)
+console.log(typeof isStudent)
+
+console.log(typeof undefinedValue)
+
+console.log(typeof nullValue)
+
+console.log(typeof mySymbol)
+
+console.log(typeof myBigInt)
+console.log(typeof myBigInt2)
 
 // 5. Imprime por consola el tipo de todas las variables
-
+// ya lo hice barba
 // 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+// var
+var helloWorld = "hola LopA"
+console.log (helloWorld)
+
+
 
 // 7. A continuación, modifica los valores de las variables por otros de distinto tipo
 
